ff720c5978eca9eff39b445707abada18632ff7a
ReyraGaming <Reynaldijones@gmail.com>
2024-01-01 11:25:21 +0800

    masih sama

---
+++ backpack.js
@@ -0,0 +1,3 @@
+export async function getBag(limit) {
+    const urlBag = "https://dummyjson.vercel.app/products/nike/backpacks?limit={limit}";
+}
---
+++ shoes.js
@@ -0,0 +1,3 @@
+export async function getShoes(limit) {
+    const urlBag = "https://dummyjson.vercel.app/products/nike/shoes?limit={limit}";
+}
